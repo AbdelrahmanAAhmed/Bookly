82bfb4a38fc3046b84c66a541681d8ede11d9e6b
AbdelrahmanAahmed <abdelrahmanabdelhameed2000@gmail.com>
2023-12-03 20:21:52 +0200

    refactor init state and navigate to home

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1test/constant.dart';
 import 'package:flutter_application_1test/core/utilis/assets.dart';
+import 'package:flutter_application_1test/features/home/presentaton/views/home_view.dart';
 import 'package:flutter_application_1test/features/splash/presentation/views/widgets/sliding_Text.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -18,6 +22,18 @@ class _SplashViewBodyState extends State<SplashViewBody>
   void initState() {
     super.initState();
 
+    animateController();
+    navigateToHome();
+  }
+
+  void navigateToHome() {
+    Future.delayed(const Duration(seconds: 2), () {
+      Get.to(() => const HomeView(),
+          transition: Transition.fade, duration: kTransitionDuration);
+    });
+  }
+
+  void animateController() {
     animationController = AnimationController(
       vsync: this,
       duration: const Duration(seconds: 1),
